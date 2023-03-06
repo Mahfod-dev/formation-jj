fffe2caab71cab49443707b9291549ae32d094c2
Mahfod <mahfod.dev@gmail.com>
2023-03-06 09:43:42 +0100

    init foramtion

---
+++ cocktails/index.js
@@ -0,0 +1,5 @@
+// le but de l'exercice est de créer une application en javascript
+//il faudra recuperer l'api sur https://www.thecocktaildb.com/api.php
+//avec un input pour filter la data et un css responsive
+
+//la meme application devrait etre créer en react
